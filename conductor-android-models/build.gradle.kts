f442a386aef83b4a1582a2a44861dfd9c47fa3b2
Dima Zaytsev <dmitry.zaicew@gmail.com>
2022-07-15 11:00:41 +0200

    Set up publishing for conductor-android-models

--- conductor-android-models/build.gradle.kts
+++ conductor-android-models/build.gradle.kts
@@ -1,10 +1,20 @@
+import com.vanniktech.maven.publish.SonatypeHost.*
+
 val ktorVersion = findProperty("ktor_version")
 
 plugins {
+    id("maven-publish")
     id("kotlin")
+    id("com.vanniktech.maven.publish")
     kotlin("plugin.serialization") version "1.6.10"
 }
 
 dependencies {
     implementation("io.ktor:ktor-serialization:$ktorVersion")
+}
+
+plugins.withId("com.vanniktech.maven.publish") {
+    mavenPublish {
+        sonatypeHost = S01
+    }
 }
