20e36ea69d6f21f3f4b5839dba96d3a1bf489067
Amanjeet Singh <amanjeetsingh150@gmail.com>
2022-12-05 16:00:11 +0530

    Run xctest with xctestrun file from `DeviceService`

--- maestro-cli/src/main/java/maestro/cli/device/ios/IOSUiTestRunner.kt
+++ maestro-cli/src/main/java/maestro/cli/device/ios/IOSUiTestRunner.kt
@@ -13,6 +13,8 @@ import java.nio.file.Files
 object IOSUiTestRunner {
 
     private const val UI_TEST_RUNNER_PATH = "/maestro-driver-iosUITests-Runner.zip"
+    private const val XCTEST_RUN_PATH = "/maestro-driver-ios-config.xctestrun"
+    private const val UI_TEST_HOST_PATH = "/maestro-driver-ios.zip"
     const val UI_TEST_RUNNER_APP_BUNDLE_ID = "dev.mobile.maestro-driver-iosUITests.xctrunner"
 
     fun install(device: Device.Connected) {
@@ -41,7 +43,48 @@ object IOSUiTestRunner {
         }
     }
 
-    fun ensureOpen(device: Device.Connected) {
-        Simctl.ensureLaunchApp(device.instanceId, UI_TEST_RUNNER_APP_BUNDLE_ID)
+    fun ensureOpen() {
+        Simctl.ensureAppAlive(UI_TEST_RUNNER_APP_BUNDLE_ID)
+    }
+
+    fun runXCTest(device: Device.Connected) {
+        val processOutput = ProcessBuilder(
+            "bash",
+            "-c",
+            "xcrun simctl spawn booted launchctl print system | grep $UI_TEST_RUNNER_APP_BUNDLE_ID | awk '/$UI_TEST_RUNNER_APP_BUNDLE_ID/ {print \$3}'"
+        ).start().inputStream.source().buffer().readUtf8().trim()
+
+        if (!processOutput.contains(UI_TEST_RUNNER_APP_BUNDLE_ID)) {
+            val tempDir = System.getenv("TMPDIR")
+            val xctestRunFile = File("$tempDir/maestro-driver-ios-config.xctestrun")
+
+            writeFileToDestination(XCTEST_RUN_PATH, xctestRunFile)
+
+            extractZipToApp("maestro-driver-iosUITests-Runner", UI_TEST_RUNNER_PATH)
+            extractZipToApp("maestro-driver-ios", UI_TEST_HOST_PATH)
+
+            Simctl.runXcTestWithoutBuild(
+                device.instanceId,
+                xctestRunFile.absolutePath
+            )
+        }
+    }
+
+    private fun extractZipToApp(appFileName: String, srcAppPath: String) {
+        val appFile = File("${System.getenv("TMPDIR")}/Debug-iphonesimulator").apply { mkdir() }
+        val appZip = File("${System.getenv("TMPDIR")}/$appFileName.zip")
+
+        writeFileToDestination(srcAppPath, appZip)
+        ArchiverFactory.createArchiver(appZip).apply {
+            extract(appZip, appFile)
+        }
+    }
+
+    private fun writeFileToDestination(srcPath: String, destFile: File) {
+        Maestro::class.java.getResourceAsStream(srcPath)?.let {
+            val bufferedSink = destFile.sink().buffer()
+            bufferedSink.writeAll(it.source())
+            bufferedSink.flush()
+        }
     }
 }
